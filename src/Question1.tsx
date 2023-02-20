13638745ffb6d877cce8511091f7003fd72a1486
Tony <tony@DESKTOP-07BVJ71>
2023-02-20 12:27:47 +0800

    fix: Adjust question 1

--- src/Question1.tsx
+++ src/Question1.tsx
@@ -1,31 +1,41 @@
 import { useState } from "react";
 
+// Simulate send order request
 const sendOrder = () => {
-  return new Promise<void>((resolve) => {
+  return new Promise<boolean>((resolve) => {
     setTimeout(() => {
-      resolve();
+      resolve(Math.random() > 0.5);
     }, 1000);
   });
 };
 
 const Question1: React.FC = () => {
-  const [orderCount, setOrderCount] = useState<number>(0);
+  const [successOrderCount, setSuccessOrderCount] = useState<number>(0);
+  const [failOrderCount, setFailOrderCount] = useState<number>(0);
   const [sendCount, setSendCount] = useState<number>(0);
 
   const handleSendOrder = async () => {
     setSendCount(sendCount + 1);
 
-    sendOrder().then(() => setOrderCount(orderCount + 1));
+    sendOrder().then((result) => {
+      if (result) {
+        setSuccessOrderCount(successOrderCount + 1)
+      } else {
+        setFailOrderCount(failOrderCount + 1)
+      }
+    });
   };
+
+  const orderCount = successOrderCount + failOrderCount
 
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-xl">
-        Question: Please make order count synced with send count
+        Question: Please make order count synced with send count eventually after smashing send order button
       </h1>
       <hr />
       <div>Send Count: {sendCount}</div>
-      <div>Order Count: {orderCount}</div>
+      <div>Order Count: {orderCount} (<span className="text-green-600">Success: {successOrderCount}</span> <span className="ml-2 text-red-600">Failed: {failOrderCount}</span>)</div>
       <button onClick={handleSendOrder} className="w-32">
         Send Order
       </button>
